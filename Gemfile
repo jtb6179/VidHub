76cc654cc170b6eb76cc95349c688b95c1f5f82f
David Cha <davidcha17@yahoo.com>
2020-03-26 13:15:41 -0400

    combine

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'rack-cors'
 gem "paperclip", ">= 5.2.0"
 
 gem 'carrierwave'
+gem 'cloudinary'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'drakoni'
+    config.api_key = '546473414755689'
+    config.api_secret = '0TIzFZJkd8mlSXxIysAxbNXpZeQ'
+    config.secure = true
+    config.cdn_subdomain = true
+end
